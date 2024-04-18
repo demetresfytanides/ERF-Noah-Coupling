5dff97c776d0032a8e3e3746921cdf9e00dbff31
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2024-04-18 15:28:54 -0500

    updates

--- simulation/Atmospheric-BL/build.sh
+++ simulation/Atmospheric-BL/build.sh
@@ -2,7 +2,9 @@
 NodeDir=$(realpath .)
 
 ERFSha="f97cda0460a6f30eea16a7b988569f4494c2181e"
+#ERFSha="19de6a62"
 
 echo Using SHA: $ERFSha
 cd $ERF_HOME && git checkout $ERFSha
-cd Exec/ABL && make -j -DERF_EXPLICIT_MOST_STRESS && cp ERF3d.gnu.TEST.TPROF.MPI.ex $NodeDir
+cd Exec/ABL && cp $NodeDir/GNUmakefile . && make clean || true
+make -j && cp ERF3d.gnu.TEST.TPROF.MPI.ex $NodeDir
