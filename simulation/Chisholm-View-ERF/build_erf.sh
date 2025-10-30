278fc570f9f659425279ddb9842a964a4483dcfc
Akash <adhruv@polaris-login-01.hsn.cm.polaris.alcf.anl.gov>
2025-10-30 21:32:40 +0000

    Save WPS test suite case for ERF

--- simulation/Chisholm-View-ERF/build_erf.sh
+++ simulation/Chisholm-View-ERF/build_erf.sh
@@ -7,11 +7,14 @@ mkdir -pv tmp_build_dir && cd tmp_build_dir
 which cmake
 cmake --version
 
+export CRAYPE_LINK_TYPE="dynamic"
+
 cmake -DCMAKE_INSTALL_PREFIX:PATH=./install \
    -DCMAKE_CXX_COMPILER:STRING=mpicxx \
    -DCMAKE_C_COMPILER:STRING=mpicc \
    -DCMAKE_Fortran_COMPILER:STRING=mpifort \
    -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
+   -DCMAKE_CUDA_HOST_COMPILER:STRING="$(which mpicxx)" \
    -DERF_DIM:STRING=3 \
    -DERF_ENABLE_MPI:BOOL=ON \
    -DERF_ENABLE_CUDA:BOOL=OFF \
@@ -22,5 +25,10 @@ cmake -DCMAKE_INSTALL_PREFIX:PATH=./install \
    -DNETCDF_DIR=$NETCDF_HOME \
    -DERF_ENABLE_NOAHMP:BOOL=ON \
    -DERF_ENABLE_HDF5:BOOL=OFF \
+   -DERF_ENABLE_EKAT:BOOL=ON \
+   -DERF_ENABLE_KOKKOS:BOOL=ON \
+   -DERF_ENABLE_RRTMGP:BOOL=ON \
    -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON \
-   $ERF_HOME && make -j10
+   $ERF_HOME && make -j
+   #-DCMAKE_BUILD_TYPE=Debug \
+   #-DCMAKE_CUDA_FLAGS="-I${MPI_HOME}/include" \
--- simulation/Chisholm-View-ERF/run_wps.sh
+++ simulation/Chisholm-View-ERF/run_wps.sh
@@ -1,4 +1,13 @@
 # cache the value of current working directory
 NodeDir=$(realpath .)
 
-mpirun -n 10 tmp_build_dir/Exec/MoistRegTests/WPS_and_Metgrid_Tests/erf_wps_test inputs_real_ChisholmView 
+NNODES=`wc -l < $PBS_NODEFILE`
+NRANKS=32 # Number of MPI ranks to spawn per node
+NDEPTH=2 # Number of hardware threads per rank (i.e. spacing between MPI ranks)
+NTHREADS=1 # Number of software threads per rank to launch (i.e. OMP_NUM_THREADS)
+
+NTOTRANKS=$(( NNODES * NRANKS ))
+
+echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_RANKS= ${NTOTRANKS} RANKS_PER_NODE= ${NRANKS} THREADS_PER_RANK= ${NTHREADS}"
+
+mpiexec --np ${NTOTRANKS} -ppn ${NRANKS} -d ${NDEPTH} --cpu-bind depth -env OMP_NUM_THREADS=${NTHREADS} tmp_build_dir/Exec/MoistRegTests/WPS_and_Metgrid_Tests/erf_wps_test inputs_real_ChisholmView
