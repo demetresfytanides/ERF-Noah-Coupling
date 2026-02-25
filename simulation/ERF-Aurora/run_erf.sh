3cb16944b44b1c2b17a20608e855d5e1bd64384a
Akash <adhruv@aurora-uan-0012.hostmgmt.cm.aurora.alcf.anl.gov>
2026-02-25 02:22:08 +0000

    add additional scaffolding for ERF on aurora

---
+++ simulation/ERF-Aurora/run_erf.sh
@@ -0,0 +1,13 @@
+# cache the value of current working directory
+NodeDir=$(realpath .)
+
+NNODES=`wc -l < $PBS_NODEFILE`
+NRANKS=4 # Number of MPI ranks to spawn per node
+NDEPTH=16 # Number of hardware threads per rank (i.e. spacing between MPI ranks)
+NTHREADS=1 # Number of software threads per rank to launch (i.e. OMP_NUM_THREADS)
+
+NTOTRANKS=$(( NNODES * NRANKS ))
+
+echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_RANKS= ${NTOTRANKS} RANKS_PER_NODE= ${NRANKS} THREADS_PER_RANK= ${NTHREADS}"
+
+mpiexec --np ${NTOTRANKS} -ppn ${NRANKS} -d ${NDEPTH} --cpu-bind depth -env OMP_NUM_THREADS=${NTHREADS} tmp_build_dir/Exec/MoistRegTests/WPS_and_Metgrid_Tests/erf_wps_test inputs_tall_towers
